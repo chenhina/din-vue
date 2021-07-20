0287043351c5c1c6cd00b648b4ebd3d68cbe803d
Hina-Rui <861472515@qq.com>
2021-07-20 18:22:16 +0800

    初始化配置

--- src/main.js
+++ src/main.js
@@ -3,10 +3,27 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '@/assets/css/global.css'
+import settings from './assets/js/settings'
+
+Vue.prototype.$settings = settings
+
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+
+import cookies from 'vue-cookies'
+Vue.prototype.$cookies = cookies
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+
+    export default {
+        name: 'Home',
+        components: {},
+        data() {
+            return {}
+        },
+        methods: {
+            initData() {
+                console.log(1111)
+                // this.$http.get(this.$settings.base_url).then(res => {
+                //     console.log(res.data)
+                // })
+            },
+        },
+        mounted() {
+            this.initData()
+        }
+    }
 </script>
